6918007e54b02a4aba86a1f3ef6b49dac268ccc3
Samuel Weissman <samuel.weissman@yahoo.com>
2019-09-16 06:20:09 -0400

    Add REPL (Read - Evaluate - Print Loop) as startup program

--- WritingAnInterpreterInCSharp/WritingAnInterpreterInCSharp/Program.cs
+++ WritingAnInterpreterInCSharp/WritingAnInterpreterInCSharp/Program.cs
@@ -6,7 +6,10 @@ namespace WritingAnInterpreterInCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string user = "user";
+            Console.WriteLine($"Hello {user}! This is the Monkey programming language!");
+            Console.WriteLine("Feel free to type in commands");
+            Repl.Repl.Start();
         }
     }
 }
---
+++ WritingAnInterpreterInCSharp/WritingAnInterpreterInCSharp/Repl/Repl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WritingAnInterpreterInCSharp.Tokens;
+
+namespace WritingAnInterpreterInCSharp.Repl
+{
+    class Repl
+    {
+        private const string prompt = ">> ";
+
+        public static void Start()
+        {
+            while(true)
+            {
+                Console.Write(prompt);
+                string input = Console.ReadLine();
+
+                if (input == string.Empty)
+                    return;
+
+                var l = new Lexer.Lexer(input);
+
+                Token token;
+
+                do
+                {
+                    token = l.NextToken();
+                    Console.WriteLine(token);
+                } while (token.Type.Equals(TokenType.EOF) == false);
+            }
+        }
+
+    }
+}
